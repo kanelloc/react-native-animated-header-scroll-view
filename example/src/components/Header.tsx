d6caa21fca95258864770262b7e48774536de4e4
Babis Kanellopoulos <babis.kane@gmail.com>
2022-12-28 13:06:19 +0200

    fix: inline styles

--- example/src/components/Header.tsx
+++ example/src/components/Header.tsx
@@ -1,32 +1,42 @@
 import { RoundButton } from './RoundButton';
-import { Text, useWindowDimensions, View } from 'react-native';
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import * as React from 'react';
 import ArrowLeft from '../icons/ArrowLeft';
 
 export const Header = () => {
   const { width } = useWindowDimensions();
   return (
-    <View
-      style={{
-        width: '100%',
-        paddingHorizontal: 8,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      }}
-    >
+    <View style={styles.container}>
       <View style={{ width: width / 3 }}>
         <RoundButton icon={<ArrowLeft />} />
       </View>
       <View
-        style={{
-          width: width / 3,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        style={[
+          styles.titleContainer,
+          {
+            width: width / 3,
+          },
+        ]}
       >
-        <Text style={{ fontWeight: 'bold' }}>Header</Text>
+        <Text style={styles.title}>Header</Text>
       </View>
       <View style={{ width: width / 3 }} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  titleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+});
--- example/src/components/OverflowHeaderComponent.tsx
+++ example/src/components/OverflowHeaderComponent.tsx
@@ -1,5 +1,5 @@
 import { RoundButton } from './RoundButton';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import * as React from 'react';
 import ArrowLeft from '../icons/ArrowLeft';
 import { ArrowRight } from '../icons/ArrowRight';
@@ -7,14 +7,7 @@ import { Share } from '../icons/Share';
 
 export const OverflowHeaderComponent = () => {
   return (
-    <View
-      style={{
-        width: '100%',
-        paddingHorizontal: 8,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      }}
-    >
+    <View style={styles.container}>
       <RoundButton icon={<ArrowLeft />} />
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <View style={{ marginRight: 8 }}>
@@ -25,3 +18,12 @@ export const OverflowHeaderComponent = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
