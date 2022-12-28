bba3b4477a671dee70a0e242f335bf8300b117e2
Babis Kanellopoulos <babis.kane@gmail.com>
2022-12-28 13:59:46 +0200

    feat: example clean up

--- example/src/screens/OverflowHeaderExample.tsx
+++ example/src/screens/OverflowHeaderExample.tsx
@@ -1,13 +1,15 @@
 import { Card, Header, OverflowHeaderComponent } from '../components';
 import { AnimatedScrollView } from '../../../src/components/AnimatedScrollView';
 import * as React from 'react';
+import { Platform } from 'react-native';
+const isIos = Platform.OS === 'ios';
 
 export const OverflowHeaderExample = () => {
   const data = Array.from(Array(20).keys());
   return (
     <AnimatedScrollView
       headerImgHeight={400}
-      headerHeight={90}
+      headerHeight={isIos ? 90 : 70}
       OverflowHeaderComponent={<OverflowHeaderComponent />}
       HeaderComponent={<Header />}
       headerImage={require('../../assets/cabin.jpg')}
