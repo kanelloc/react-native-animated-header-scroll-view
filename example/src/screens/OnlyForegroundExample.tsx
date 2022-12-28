890b891c9908b73211d55277441b164bc0d906ad
Babis Kanellopoulos <babis.kane@gmail.com>
2022-12-29 01:17:26 +0200

    feat: setup example

---
+++ example/src/screens/OnlyForegroundExample.tsx
@@ -0,0 +1,20 @@
+import { AnimatedScrollView } from '@kanelloc/react-native-animated-header-scroll-view';
+import { Card, Header, OverflowHeaderComponent } from '../components';
+import * as React from 'react';
+import { ForegroundComponent } from '../components/ForegroundComponent';
+import { data, isIOS } from '../utils';
+
+export const OnlyForegroundExample = () => {
+  return (
+    <AnimatedScrollView
+      ForegroundComponent={<ForegroundComponent />}
+      headerHeight={isIOS ? 90 : 70}
+      OverflowHeaderComponent={<OverflowHeaderComponent />}
+      HeaderComponent={<Header />}
+    >
+      {data.map((e) => {
+        return <Card item={e} key={e} />;
+      })}
+    </AnimatedScrollView>
+  );
+};
